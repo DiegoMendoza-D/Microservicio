b81e437e8fdedc8c48f5cee0650be920bb1e719e
root <root@DiegoMendoza.>
2025-02-20 20:35:01 -0600

    configure

--- usuarios/src/controllers/usuarios.controller.ts
+++ usuarios/src/controllers/usuarios.controller.ts
@@ -1,5 +1,8 @@
-export const getAll = (req:any,res: any) =>{
-    res.status(200).json({
-        mensaje: "Bienvenido usuario",
-    })
+export const getAll = (req: any, res: any) => {
+    const doctores = [
+        { id: 1, nombre: "Dr. Ana López", especialidad: "Neurología" },
+        { id: 2, nombre: "Dr. Juan Osorio", especialidad: "Traumatología" }
+    ];
+
+    res.status(200).json(doctores);
 }
